5626a0b879ec7f6cca1a33f9454da43fe245439f
EngTarcisioSM <dev.engtsm@gmail.com>
2022-09-16 07:55:08 -0300

    att 004

--- 002_Rest-Crud-Memoria/app.py
+++ 002_Rest-Crud-Memoria/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis
+from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Hoteis, '/hoteis')
+
+#2. É inserido aqui o nome recurso, como o proximo parametro deve ser uma string
+# é colocado <string>:holtel_id
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+
 
 if __name__ == '__main__':
 
--- 002_Rest-Crud-Memoria/resources/hotel.py
+++ 002_Rest-Crud-Memoria/resources/hotel.py
@@ -1,4 +1,6 @@
-from flask_restful import Resource
+#1. É necessário incluir a biblioteca reqparse para poder receber dados de
+# requisições
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -29,3 +31,49 @@ class Hoteis(Resource):
     def get(self):
 
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+
+    def get(self, hotel_id):
+
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+
+        return {'message': 'Hotel not found'}, 404
+
+    def post(self, hotel_id):
+        #2. Os dados recebidos ficam alocados dentro de reqparse.RequestParser()
+        #o dado de hotel_id não virá como argumento
+        argumentos = reqparse.RequestParser()
+        #3. Define os argumentos recebidos que serão aceitos, caso sejam enviados
+        #outros eses serão descartados na logica do programa.
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        #4. passara para a variavel dados um conjunto de chave valor conforme
+        #os dados especificados acima
+        dados = argumentos.parse_args()
+
+        #5. criada a estrutura a ser inserida dentro do "banco de dados" existente
+        #hoje que seria hoteis
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+        hoteis.append(novo_hotel)
+
+        #6. Retorno do hotel criado com o código de sucesso da requisição o
+        #código 200
+        return novo_hotel, 200
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
