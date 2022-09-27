a10b296cb19085d3320ffca4bf6d941ac6be8250
EngTarcisioSM <dev.engtsm@gmail.com>
2022-09-27 17:30:36 -0300

    ainda não acabou

--- Autenticacao_Usuario/models/usuario.py
+++ Autenticacao_Usuario/models/usuario.py
@@ -8,15 +8,23 @@ class UserModel(banco.Model):
     user_id = banco.Column(banco.Integer, primary_key=True)
     login = banco.Column(banco.String(40))
     senha = banco.Column(banco.String(40))
+    # 1. incluso o campo para verificação se o usuário ja foi ativado, será do 
+    # tipo boolean e por default será dado como false, desta forma mesmo que o 
+    # usuário não preencha esse campo terá um valor inicial de "False" 
+    ativado = banco.Column(banco.Boolean, default=False)
 
-    def __init__(self, login, senha) -> None:
+    # 2. Adicionar o seu recebimento dentro do inicializador 
+    def __init__(self, login, senha, ativado) -> None:
         self.login = login
         self.senha = senha
+        self.ativado = ativado 
 
+    # 3. acrescentado no JSON
     def json(self):
         return {
             'user_id': self.user_id,
-            'login': self.login
+            'login': self.login,
+            'ativado': self.ativado
         }
 
     @classmethod
--- Autenticacao_Usuario/resources/usuario.py
+++ Autenticacao_Usuario/resources/usuario.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import boolean
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
@@ -9,6 +10,8 @@ atributos.add_argument('login', type=str, required=True, help="The \
             'login' field cannot be left blank")
 atributos.add_argument('senha', type=str, required=True, help="The \
             'password' field cannot be left blank")
+# 4. inserir mais este campo a ser capturado ao receber a requisição, não sendo obrigatorio seu preenchimento
+atributos.add_argument('ativado', type=boolean)
 
 
 class User(Resource):
@@ -42,6 +45,11 @@ class UserRegister(Resource):
                     .format(dados['login'])}
 
         user = UserModel(**dados)
+        # 5. para garantir que nenhum usuario malicioso efetue a ativação
+        # enviando true, será forçado aqui o valor de ativado para false para
+        # que não ocorra problemas com a logica implementada
+        user.ativado = False
+
         user.save_user()
 
         return {"message": "User create successfully!"}, 201
@@ -55,10 +63,13 @@ class UserLogin(Resource):
         user = UserModel.find_by_login(dados['login'])
 
         if user and safe_str_cmp(user.senha, dados['senha']):
-            token_acesso = create_access_token(identity=user.user_id)
-
-            return {'acess_token': token_acesso}, 200
-
+            # 6. alem da checagem acima de verificar se o usuario e senha
+            # estão iguais, será verificado também aqui se o usuario teve sua
+            # conta ativada
+            if user.ativado:
+                token_acesso = create_access_token(identity=user.user_id)
+                return {'acess_token': token_acesso}, 200
+            return {'message': 'User not confirmed.'}, 400
         return {'message': 'The username or password is incorrect.'}, 401
 
 
@@ -72,3 +83,23 @@ class UserLogout(Resource):
 
         return {'message': 'Logout successfully!'}, 200
 
+
+# 7. Criação de um recurso que efetua a ativação do usuario
+class UserConfirm(Resource):
+
+    # 8. Será um método de classe, não necessitando criar um usuário
+    @classmethod
+    def get(cls, user_id):
+        # 9. tenta localizar o usuario atraves do id
+        user = UserModel.find_user(user_id)
+
+        if not user:
+            return {'message': "User id '{}' not found.".format(user_id)}, 404
+
+        # 10. passo pelo if de cima logo usuario foi encontrado devendo ser
+        # ativado
+        user.ativado = True
+        # 11. Atualizando o status do usuario
+        user.save_user()
+        return {'message': "User id '{}' confirmed successfully."
+                .format(user_id)}
